a9f4bd2eba796e450589bc1391f8376d9abdf1ce
Setup <Setup@EPINHYDW07CA.princeton.epam.com>
2021-03-08 12:51:25 +0530

    confi server

---
+++ src/main/java/myhotel/configServer/MyHotelConfigServerApplication.java
@@ -0,0 +1,13 @@
+package myhotel.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHotelConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHotelConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myhotel/configServer/MyHotelConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package myhotel.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHotelConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
